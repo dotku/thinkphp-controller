05f414300ce2c22f84f2ef92e5ce4b9ea6054080
Jay Lin <weijingjaylin@gmail.com>
2016-03-23 19:31:22 -0700

    first commit

---
+++ Home/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    // ThinkPHP SETTINGS
+    
+    //'MODULE_ALLOW_LIST'     => array('Home','Stat','Api', 'Common', 'Gear', 'Dev'),
+    'DEFAULT_MODULE'        =>  'Home',  // 默认模块
+    'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
+);
